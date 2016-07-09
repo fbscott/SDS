83e6a3fa9fa4c925f8d4c8475daee009de57fbea
Scott <scottcurrell@gmail.com>
2016-07-08 21:30:41 -0600

    Begin footer - WIP

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+;(function($) {
+			// Copyright span
+	var copyright = document.getElementsByClassName('copyright')[0],
+			// Current year
+			currentYear = new Date().getFullYear();
+	// Set copyright span to current year
+	copyright.innerHTML = currentYear;
+})(jQuery);
